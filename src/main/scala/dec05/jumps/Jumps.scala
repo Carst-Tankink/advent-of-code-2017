547a0efc3c83052f7e526b48351f899a1837113c
Carst Tankink <ctankink@bol.com>
2017-12-09 12:49:34 +0100

    AoC-2017 A more efficient implementation of jumping.
    From several hours to a couple of seconds O_O

--- src/main/scala/dec05/jumps/Jumps.scala
+++ src/main/scala/dec05/jumps/Jumps.scala
@@ -2,25 +2,49 @@ package dec05.jumps
 
 import scala.io.Source
 
+case class Tape(left: List[Int], head: Option[Int], right: List[Int])
+
+
 object Jumps {
+
+  def moveLeft(tape: Tape): Tape =
+    if (tape.left.isEmpty && tape.head.isEmpty) tape
+    else if (tape.left.isEmpty) Tape(tape.left, None, tape.head.get :: tape.right)
+    else Tape(tape.left.tail, tape.left.headOption, tape.head.get :: tape.right)
+
+  def moveRight(tape: Tape): Tape =
+    if (tape.right.isEmpty && tape.head.isEmpty) tape
+    else if (tape.right.isEmpty) Tape(tape.head.get :: tape.left, None, tape.right)
+    else
+      Tape(tape.head.get :: tape.left, tape.right.headOption, tape.right.tail)
+
+  def move(tape: Tape, jump: Int): Tape = {
+    if (jump == 0) tape
+    else if (jump < 0) move(moveLeft(tape), jump + 1)
+    else move(moveRight(tape), jump - 1)
+  }
+
   def run(instructions: Seq[Int], update: Int => Int): Long = {
-    def rec(idx: Int, instr: Seq[Int], steps: Long): Long = {
-      if (idx < 0 || idx >= instr.length) steps
+    def rec(idx: Int, instr: Tape, steps: Long): Long = {
+      if (instr.head.isEmpty) steps
       else {
-        val jump = instr(idx)
-        val newInstr = instr.updated(idx, update(jump))
-        rec(idx + jump, newInstr, steps + 1)
+        val jump = instr.head.get
+        val newHead = update(jump)
+        val newTape = move(Tape(instr.left, Some(newHead), instr.right), jump)
+        rec(idx + jump, newTape, steps + 1)
       }
     }
-    rec(0, instructions, 0)
+
+    val t: Tape = Tape(List.empty, instructions.headOption, instructions.tail.toList)
+    rec(0, t, 0)
   }
 
   def main(args: Array[String]): Unit = {
     val instructions: Seq[Int] = Source.fromFile("input").getLines().map(s => s.toInt).toSeq
     val inc: (Int) => Int = (x: Int) => x + 1
 
-//    val steps = run(instructions, inc)
-    val steps =  run(instructions, (x: Int) => if(x >= 3) x-1 else x+1)
+//        val steps = run(instructions, inc)
+    val steps = run(instructions, (x: Int) => if (x >= 3) x - 1 else x + 1)
     println(steps)
 
   }
