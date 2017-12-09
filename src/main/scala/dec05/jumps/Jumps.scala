9d56b8ec6229b5778a4ff63dbc91c0d69dbb0308
Carst Tankink <ctankink@bol.com>
2017-12-09 10:45:02 +0100

    AoC-2017 05/12 Part two
    Takes quite a while, wonder if this is due to inefficient list updates?

--- src/main/scala/dec05/jumps/Jumps.scala
+++ src/main/scala/dec05/jumps/Jumps.scala
@@ -3,14 +3,12 @@ package dec05.jumps
 import scala.io.Source
 
 object Jumps {
-
-
-  def run(instructions: Seq[Int]): Int = {
-    def rec(idx: Int, instr: Seq[Int], steps: Int): Int = {
+  def run(instructions: Seq[Int], update: Int => Int): Long = {
+    def rec(idx: Int, instr: Seq[Int], steps: Long): Long = {
       if (idx < 0 || idx >= instr.length) steps
       else {
         val jump = instr(idx)
-        val newInstr = instr.updated(idx, jump + 1)
+        val newInstr = instr.updated(idx, update(jump))
         rec(idx + jump, newInstr, steps + 1)
       }
     }
@@ -19,8 +17,10 @@ object Jumps {
 
   def main(args: Array[String]): Unit = {
     val instructions: Seq[Int] = Source.fromFile("input").getLines().map(s => s.toInt).toSeq
-    val steps = run(instructions)
+    val inc: (Int) => Int = (x: Int) => x + 1
 
+//    val steps = run(instructions, inc)
+    val steps =  run(instructions, (x: Int) => if(x >= 3) x-1 else x+1)
     println(steps)
 
   }
