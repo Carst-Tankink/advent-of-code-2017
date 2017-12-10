0ba34c3c3f81caab10e641d69f1870345d7e605a
Carst Tankink <ctankink@bol.com>
2017-12-10 15:50:01 +0100

    AoC-2017 Day 9 part two

--- src/main/scala/dec09/streams/Streams.scala
+++ src/main/scala/dec09/streams/Streams.scala
@@ -2,31 +2,25 @@ package dec09.streams
 
 import scala.io.StdIn
 
-case class Stream(parent: Option[Stream], children: List[Stream]) {
-  def addedChild(s: Stream): Stream = {
-    Stream(parent, s :: children)
-  }
+case class Stream(parent: Option[Stream], children: List[Stream], garbage: Int) {
+  def addedChild(s: Stream): Stream = copy(children = s :: children)
 
-  override def toString: String = {
-    "{" + children.mkString + "}"
-  }
+  def addedGarbage(): Stream = copy(garbage = garbage + 1)
 }
 
 case class ParserState(current: Option[Stream], garbage: Boolean, canceling: Boolean)
 
 object Streams {
-
-
   def nextState(state: ParserState, c: Char): ParserState =
     if (state.canceling) ParserState(state.current, garbage = state.garbage, canceling = false)
     else if (state.garbage) c match {
       case '>' => ParserState(state.current, garbage = false, canceling = state.canceling)
       case '!' => ParserState(state.current, garbage = state.garbage, canceling = true)
-      case _ => state
+      case _ => ParserState(state.current.map(_.addedGarbage()), garbage = state.garbage, canceling = state.canceling)
     }
     else c match {
       case '{' =>
-        val stream = Stream(state.current, List.empty)
+        val stream = Stream(state.current, List.empty, 0)
         ParserState(Some(stream), state.garbage, state.canceling)
       case '}' =>
         val parent = state.current.flatMap(_.parent).map(p => p.addedChild(state.current.get))
@@ -36,7 +30,7 @@ object Streams {
     }
 
   def parseStream(input: String): Option[Stream] = {
-    val parsed = input.foldLeft(ParserState(Some(Stream(None, List.empty)), garbage = false, canceling = false))((state, c) => nextState(state, c))
+    val parsed = input.foldLeft(ParserState(Some(Stream(None, List.empty, 0)), garbage = false, canceling = false))((state, c) => nextState(state, c))
     parsed.current
   }
 
@@ -49,15 +43,21 @@ object Streams {
     rec(1, s)
   }
 
+  def sumGarbage(stream: Stream): Int = {
+    stream.garbage + stream.children.map(s => sumGarbage(s)).sum
+  }
+
   def main(args: Array[String]): Unit = {
     while (true) {
       println("Input")
       val input: String = StdIn.readLine()
       val stream: Stream = parseStream(input).get.children.head
-      println("Stream: " + stream)
-
       val score = calculateScore(stream)
       println("Score: " + score)
+
+      val garbage = sumGarbage(stream)
+      println("Garbage: " + garbage{})
+
     }
   }
 }
