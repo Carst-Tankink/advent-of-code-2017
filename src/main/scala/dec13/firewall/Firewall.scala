1aa5b7c3d8b35cc4624e3d7f55625334bdd0d1c6
Carst Tankink <ctankink@bol.com>
2017-12-13 15:18:17 +0100

    AoC-2017 Day 13 part two. Had to abstract using the reddit answers

--- src/main/scala/dec13/firewall/Firewall.scala
+++ src/main/scala/dec13/firewall/Firewall.scala
@@ -4,55 +4,35 @@ import scala.io.Source
 
 sealed trait Dir
 
-case object Up extends Dir
-
-case object Down extends Dir
-
-case class Layer(scanner: Int, scannerDir: Dir, depth: Int, range: Int) {
-  def next: Layer = {
-    val updateVal = if (scannerDir == Up) -1 else 1
-    val newScanner = scanner + updateVal
-
-    val newDir = if (newScanner == range - 1) Up else if (newScanner == 0) Down else scannerDir
-    copy(scanner = newScanner, newDir)
+case class Layer(depth: Int, range: Int) {
+  def caught(delay: Int): Boolean = {
+    (depth + delay) % (2 * range - 2) == 0
   }
 }
 
-case class PassageState(accumulatedSeverity: Int, layers: List[Layer])
 
 object Firewall {
 
   def toLayer(str: String): Layer = {
     val tokens = str.split(": ").map(_.toInt)
-    Layer(0, Down, tokens.head, tokens.tail.head)
-  }
-
-  def getSeverity(pos: Int, s: PassageState): Int = {
-    s.layers
-      .filter(l => l.scanner == 0)
-      .filter(l => {
-        val depth = l.depth
-        depth == pos
-      })
-      .map(l => {
-        l.depth * l.range
-      })
-      .headOption
-      .getOrElse(0)
+    Layer(tokens.head, tokens.tail.head)
   }
 
   def passThrough(layers: List[Layer]): Int = {
-    val maxDepth = layers.map(_.depth).max
-    List.range(0, maxDepth + 1).foldLeft(PassageState(0, layers))((s, pos) => {
-      val severity = getSeverity(pos, s)
-      val stepScanners = s.layers.map(l => l.next)
-      PassageState(s.accumulatedSeverity + severity, stepScanners)
-    }).accumulatedSeverity
+    layers.filter(_.caught(0)).map(l => l.depth * l.range).sum
+  }
+
+
+  def calculateSafe(layers: List[Layer]) : Int = {
+    Stream.from(0).filter(i => layers.forall(l => !l.caught(i))).head
   }
 
   def main(args: Array[String]): Unit = {
     val layers: List[Layer] = Source.fromFile("input").getLines.map(toLayer).toList
     val severity = passThrough(layers)
     println("Severity: " + severity)
+
+    val safe = calculateSafe(layers)
+    println("Safe delay: " + safe)
   }
 }
