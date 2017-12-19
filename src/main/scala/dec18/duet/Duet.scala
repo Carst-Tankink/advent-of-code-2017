6e85b424ce99eb31800389eb9c434b8fe7a2ddf4
Carst Tankink <ctankink@bol.com>
2017-12-19 08:07:31 +0100

    AoC-2017 Day 18 part one refactor elements for part two

--- src/main/scala/dec18/duet/Duet.scala
+++ src/main/scala/dec18/duet/Duet.scala
@@ -2,62 +2,55 @@ package dec18.duet
 
 import scala.io.Source
 
-case class State(register: Map[String, Long], lastPlayed: Option[Long], position: Long, done: Boolean) {
-  private def dereference(y: String): Long = {
-    try {
-      y.toLong
-    } catch {
-      case (_: NumberFormatException) => register.getOrElse(y, 0)
-    }
-  }
-
-  def updated(reg: String, v: Long): State = {
-    copy(register + (reg -> v), position = position + 1)
+case class Register(private val content: Map[String, Long] = Map.empty) {
+  def updated(reg: String, v: Long): Register = {
+    copy(content + (reg -> v))
   }
 
   def get(r: String): Long = {
-    dereference(r)
+    try {
+      r.toLong
+    } catch {
+      case (_: NumberFormatException) => content.getOrElse(r, 0)
+    }
   }
+
 }
 
-sealed trait Instruction {
-  def apply(state: State): State
+case class State(register: Register, lastPlayed: Option[Long], position: Long, done: Boolean) {
+  def get(x: String): Long = register.get(x)
+
+  def updated(x: String, v: Long): State = this.copy(register = register.updated(x, v), position = position + 1)
 }
 
-case class Snd(x: String) extends Instruction {
-  override def apply(state: State): State = State(state.register, Some(state.get(x)), state.position + 1, done = false)
-}
+sealed trait Instruction
 
-case class Set(x: String, y: String) extends Instruction {
-  override def apply(state: State): State = state.updated(x, state.get(y))
-}
+case class Snd(x: String) extends Instruction
 
-case class Add(x: String, y: String) extends Instruction {
-  override def apply(state: State): State = state.updated(x, state.get(x) + state.get(y))
-}
+case class Set(x: String, y: String) extends Instruction
 
-case class Mul(x: String, y: String) extends Instruction {
-  override def apply(state: State): State = state.updated(x, state.get(x) * state.get(y))
-}
+case class Add(x: String, y: String) extends Instruction
 
-case class Mod(x: String, y: String) extends Instruction {
-  override def apply(state: State): State = state.updated(x, state.get(x) % state.get(y))
-}
+case class Mul(x: String, y: String) extends Instruction
 
-case class Rcv(x: String) extends Instruction {
-  override def apply(state: State): State = {
-    if (state.get(x) != 0) state.copy(done = true) else state.copy(position = state.position + 1)
-  }
-}
+case class Mod(x: String, y: String) extends Instruction
 
-case class Jgz(x: String, y: String) extends Instruction {
-  override def apply(state: State): State = {
-    val jump = if (state.get(x) > 0) state.get(y) else 1
-    state.copy(position = state.position + jump)
-  }
-}
+case class Rcv(x: String) extends Instruction
+
+case class Jgz(x: String, y: String) extends Instruction
 
 object Duet {
+  def music(instruction: Instruction, state: State): State = instruction match {
+    case Snd(x) => State(state.register, Some(state.get(x)), state.position + 1, done = false)
+    case Set(x, y) => state.updated(x, state.get(y))
+    case Add(x, y) => state.updated(x, state.get(x) + state.get(y))
+    case Mul(x, y) => state.updated(x, state.get(x) * state.get(y))
+    case Mod(x, y) => state.updated(x, state.get(x) % state.get(y))
+    case Rcv(x) => if (state.get(x) != 0) state.copy(done = true) else state.copy(position = state.position + 1)
+    case Jgz(x, y) =>
+      val jump = if (state.get(x) > 0) state.get(y) else 1
+      state.copy(position = state.position + jump)
+  }
 
   def parseInstruction(s: String): Instruction = {
     val tokens = s.split(' ')
@@ -76,10 +69,18 @@ object Duet {
   def play(instructions: List[Instruction]): State = {
     def rec(s: State): State = {
       if (s.done) s
-      else rec(instructions(s.position.toInt)(s))
+      else {
+        val position = s.position.toInt
+        if (position < 0 || position > instructions.length) {
+          s.copy(done = true)
+        }
+        else {
+          rec(music(instructions(position), s))
+        }
+      }
     }
 
-    rec(State(Map.empty, None, 0, done = false))
+    rec(State(Register(), None, 0, done = false))
   }
 
   def main(args: Array[String]): Unit = {
