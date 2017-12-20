6a1c825a5c533c0ecd02c0d4305da4b55085bbc6
Carst Tankink <ctankink@bol.com>
2017-12-20 19:00:57 +0100

    AoC-2017 Day 20 part two, by guessing an iteration limit.
    Nicer would be to calculate a stop condition based on increasing
    distances between pairs, or calculate intersections of curves

--- src/main/scala/dec20/particles/Particles.scala
+++ src/main/scala/dec20/particles/Particles.scala
@@ -3,15 +3,28 @@ package dec20.particles
 import scala.io.Source
 
 case class Vector(x: Long, y: Long, z: Long) {
+  def manhattanDistance(pos: Vector): Long = {
+    Math.abs(x - pos.x) + Math.abs(y - pos.y) + Math.abs(z - pos.z)
+  }
+
   def manhattanValue: Long = Math.abs(x) + Math.abs(y) + Math.abs(z)
+
+  def +(v: Vector): Vector = {
+    Vector(x + v.x, y + v.y, z + v.z)
+  }
 }
-case class Particle(id: Int, pos: Vector, velocity: Vector, acceleration: Vector)
+
+case class Particle(id: Int, pos: Vector, velocity: Vector, acceleration: Vector) {
+  def move(): Particle = {
+    val newV = velocity + acceleration
+    copy(pos = pos + newV, velocity = newV)
+  }
+}
 
 object Particles {
 
   def parseVec(str: String): Vector = {
     val vec = str.split('=')(1).replace("<", "").replace(">", "").split(",")
-
     Vector(vec(0).toInt, vec(1).toInt, vec(2).toInt)
   }
 
@@ -23,12 +36,35 @@ object Particles {
     Particle(i, p, v, a)
   }
 
+  def simulateCollisions(input: List[Particle], steps: Int): List[Particle] = {
+    val withoutCollisions: List[Particle] = removeCollisions(input)
+    if (steps == 10000) withoutCollisions
+    else {
+      val next = withoutCollisions.map(_.move())
+      simulateCollisions(next, steps + 1)
+    }
+  }
+
+
+  private def removeCollisions(input: List[Particle]): List[Particle] = {
+    val vectorToParticles = input
+      .groupBy(_.pos)
+
+    vectorToParticles
+      .values
+      .filter(_.size == 1)
+      .flatten
+      .toList
+  }
+
   def main(args: Array[String]): Unit = {
     val input: List[Particle] = Source.fromFile("input").getLines().zipWithIndex.map(p => parseParticle(p._2, p._1)).toList
 
     val minAcc = input.map(x => x.acceleration.manhattanValue).min
     val slowest = input.filter(p => p.acceleration.manhattanValue == minAcc)
-
     println("Slowest: " + slowest)
+
+    val remainder = simulateCollisions(input, 0)
+    println("Remainder: " + remainder.size)
   }
 }
