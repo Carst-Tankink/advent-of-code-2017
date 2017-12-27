1848677be3c68081ddf43226625a8cb2af636c1d
Carst Tankink <ctankink@bol.com>
2017-12-27 13:05:22 +0100

    AoC-2017 Day 22 part two

--- src/main/scala/dec22/virus/Virus.scala
+++ src/main/scala/dec22/virus/Virus.scala
@@ -5,8 +5,9 @@ import scala.io.Source
 sealed trait Status
 
 case object Clean extends Status
-
+case object Weakened extends Status
 case object Infected extends Status
+case object Flagged extends Status
 
 sealed class Direction(val vector: (Int, Int))
 
@@ -19,10 +20,6 @@ case object Down extends Direction(0, 1)
 case object Left extends Direction(-1, 0)
 
 case class Carrier(pos: (Int, Int), dir: Direction, burst: Int, infections: Int, status: Map[(Int, Int), Status]) {
-  def updatedStatus: Map[(Int, Int), Status] = {
-    val newStatus = if (currentStatus == Clean) Infected else Clean
-    status.updated(pos, newStatus)
-  }
 
   def currentStatus: Status = status.getOrElse(pos, Clean)
 }
@@ -42,14 +39,63 @@ object Virus {
     case Right => Up
   }
 
+  def reverse(d: Direction): Direction = d match {
+    case Up => Down
+    case Down => Up
+    case Left => Right
+    case Right => Left
+  }
+
   def run(bursts: Int, statusMap: Map[(Int, Int), Status]): Carrier = {
+
     def rec(carrier: Carrier): Carrier = {
+      def updatedStatus: Map[(Int, Int), Status] = {
+        val newStatus = if (carrier.currentStatus == Clean) Infected else Clean
+        carrier.status.updated(carrier.pos, newStatus)
+      }
+
       if (carrier.burst == bursts) carrier
       else {
         val newDir = if (carrier.currentStatus == Infected) turnRight(carrier.dir) else turnLeft(carrier.dir)
-        val newStatuses = carrier.updatedStatus
+        val newStatuses = updatedStatus
         val newPos = (carrier.pos._1 + newDir.vector._1, carrier.pos._2 + newDir.vector._2)
         val newInfections = carrier.infections + (if (carrier.currentStatus == Infected) 0 else 1)
+        val newCarrier = Carrier(newPos, newDir, carrier.burst + 1, newInfections, newStatuses)
+        rec(newCarrier)
+      }
+    }
+
+    rec(Carrier((0, 0), Up, 0, 0, statusMap))
+  }
+
+  def nextStatus(status: Status): Status = status match {
+    case Clean => Weakened
+    case Weakened => Infected
+    case Infected => Flagged
+    case Flagged => Clean
+  }
+  def runEvolved(bursts: Int, statusMap: Map[(Int, Int), Status]): Carrier = {
+
+    def rec(carrier: Carrier): Carrier = {
+      def updatedStatus: Map[(Int, Int), Status] = {
+        val newStatus = nextStatus(carrier.currentStatus)
+        carrier.status.updated(carrier.pos, newStatus)
+      }
+
+      if (carrier.burst == bursts) carrier
+      else {
+        val newDir = carrier.currentStatus match {
+          case Clean => turnLeft(carrier.dir)
+          case Weakened => carrier.dir
+          case Infected => turnRight(carrier.dir)
+          case Flagged => reverse(carrier.dir)
+        }
+        val newStatus = nextStatus(carrier.currentStatus)
+        val newStatuses = carrier.status.updated(carrier.pos, newStatus)
+
+        val newPos = (carrier.pos._1 + newDir.vector._1, carrier.pos._2 + newDir.vector._2)
+
+        val newInfections = carrier.infections + (if (newStatus == Infected) 1 else 0)
         val newCarrier = Carrier(newPos, newDir, carrier.burst + 1, newInfections, newStatuses)
         rec(newCarrier)
       }
@@ -80,9 +126,11 @@ object Virus {
       .flatMap { case (l, y) => l.map { case (s, x) => (x - horizontalOffset, y - verticalOffset) -> s } }
       .toMap
 
-    val finalStatus = run(10000, statusMap)
+//    val finalStatus = run(10000, statusMap)
 
-    println("Infections: " + finalStatus.infections)
+//    println("Infections: " + finalStatus.infections)
 
+    val finalStatusEvolved = runEvolved(10000000, statusMap)
+    println("Infections: " + finalStatusEvolved.infections)
   }
 }
